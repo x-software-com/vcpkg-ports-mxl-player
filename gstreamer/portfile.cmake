735276e537377d03239a845b91066d9e1ec36b09
marcbull <31337222+marcbull@users.noreply.github.com>
2024-06-24 14:24:24 +0200

    chore: add patch to support MXL VFR

--- gstreamer/portfile.cmake
+++ gstreamer/portfile.cmake
@@ -15,6 +15,7 @@ vcpkg_from_gitlab(
         fix-mxl-compositor-seeking.patch
         fix-mxl-gst-play-seek.patch
         fix-mxl-v1-tags.patch
+        fix-mxl-baseparse-vfr.patch
         fix-clang-cl.patch
         # fix-clang-cl-gstreamer.patch
         # fix-clang-cl-base.patch
